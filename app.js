0cda72a25d1fb51960c41e5d3e4eab31d5c0bbf1
Nick <nikitalytvynov0506@gmail.com>
2024-01-30 20:37:27 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var formRouter = require("./routes/form");
+
+var app = express();
+
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.get("/test-route", (req, res) => {
+  res.send("Test route");
+});
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+const port = 3000;
+
+app.post("/submit-form", (req, res) => {
+  const { name, email, message } = req.body;
+  console.log(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`);
+  res.send("Form submitted successfully!");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
+
+module.exports = app;
